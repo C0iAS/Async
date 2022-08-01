6c98407a64d1f02fda484b9b3716ccc9bd37830d
C0iAS <sebapl09@gmail.com>
2022-07-31 20:09:46 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -23,10 +23,10 @@ async function fetchData(urlApi) {
     ${videos.items.map(video => `
       <div class="group relative">
         <div
-          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none gray">
           <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
-        <div class="mt-4 flex justify-between">
+        <div class="mt-4 flex justify-between gray">
           <h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"></span>
             ${video.snippet.title}
@@ -40,3 +40,65 @@ async function fetchData(urlApi) {
     console.log(error);
   }
 })();
+
+const aboutButton = document.querySelector('.boton-about');
+const about = document.getElementById('p2e');
+const videos = document.querySelector('.videos');
+const homeButton = document.getElementById('home');
+
+aboutButton.addEventListener('click', aboutSection);
+homeButton.addEventListener('click', aboutSection)
+
+function aboutSection() {
+  videos.classList.toggle('inactive');
+  homeButton.classList.toggle('inactive');
+  aboutButton.classList.toggle('inactive');
+  about.classList.toggle('inactive');
+}
+
+//DARK MODE
+
+let relative = [];
+relative = document.querySelectorAll('.relative');
+let white = [];
+white = document.querySelectorAll('.bg-white');
+
+const darkmode = document.getElementById('darkmode');
+const lightmode = document.getElementById('lightmode');
+
+darkmode.addEventListener('click', darkLightMode);
+lightmode.addEventListener('click', darkLightMode2);
+
+function darkLightMode() {
+  for(let i = 0; i < relative.length; i++) {
+
+    relative[i].classList.add('mode');
+    
+  }
+  for(let i = 0; i < white.length; i++) {
+
+    white[i].classList.add('mode');
+    
+  }
+  about.classList.add('mode');
+  darkmode.classList.toggle('inactive');
+  lightmode.classList.remove('inactive');
+}
+
+function darkLightMode2() {
+  for(let i = 0; i < relative.length; i++) {
+
+    relative[i].classList.remove('mode');
+    
+  }
+  for(let i = 0; i < white.length; i++) {
+
+    white[i].classList.remove('mode');
+    
+  }
+  lightmode.classList.toggle('inactive');
+  darkmode.classList.toggle('inactive');
+  about.classList.toggle('mode');
+}
+
+
